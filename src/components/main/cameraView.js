cda111a5c3eccad4400fa20faae5bc87e0429c45
NilsB2911 <63862156+NilsB2911@users.noreply.github.com>
2021-07-21 17:10:44 +0200

    awaaayy with webrtc

--- src/components/main/cameraView.js
+++ src/components/main/cameraView.js
@@ -1,18 +1,8 @@
-import {useEffect, useRef, useState} from "react";
+import {useEffect, useRef} from "react";
 
 export default function CameraView(props) {
 
     const videoRef = useRef(null);
-
-    const getVideo = () => {
-        navigator.mediaDevices.getUserMedia({
-            video: {
-                height: 1080, width: 1920
-            }
-        }).then(stream => {
-            setRefSource(stream)
-        }).catch(err => console.log(err))
-    }
 
     const setRefSource = (stream) => {
         let video = videoRef.current;
@@ -24,7 +14,7 @@ export default function CameraView(props) {
         if (props.stream) {
             setRefSource(props.stream)
         } else {
-            getVideo()
+            console.log("no prop")
         }
     }, [videoRef]);
 
--- src/components/main/camerabar.js
+++ src/components/main/camerabar.js
@@ -1,6 +1,7 @@
-import React, {Component, createRef} from 'react';
+import React, {Component} from 'react';
 import {observer} from "mobx-react";
-import CameraView from "./cameraView";
+import {Input, Button} from "semantic-ui-react";
+//import CameraView from "./cameraView";
 
 //Keine Ahnung, warum das hier notwendig war
 import {withRouter} from 'react-router-dom';
@@ -13,20 +14,42 @@ class Camerabar extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            myWeb: null,
             allUserInRoom: [],
-            streams: createRef([])
+            msg: null,
+            allMsgs: []
         }
+
     }
 
     componentDidMount() {
+        /*navigator.mediaDevices.getUserMedia({
+            video: {
+                height: 1080, width: 1920
+            }
+        }).then(stream => {
+            this.setState({myWeb: stream})
+        }).catch(err => console.log(err))*/
+
         store.socket.on("newUsernames", (array) => {
-            this.setState({allUserInRoom: array})
+            this.setState({allUserInRoom: array}, () => {
+                console.log(this.state.allUserInRoom)
+            })
         })
     }
 
     leaveRoom = () => {
         store.setRoomId(null)
         this.props.history.push("/");
+    }
+
+    handleMsg = (e) => {
+        this.setState({msg: e.target.value});
+    }
+
+    submitToSockets = () => {
+        console.log(this.state.msg);
+        this.setState({msg: null})
     }
 
     render() {
@@ -37,12 +60,24 @@ class Camerabar extends Component {
                         {this.state.allUserInRoom.map((userObj, index) => {
                             return (
                                 <div className={"videoNameWrapper"} key={index}>
-                                    <CameraView/>
+                                    {/*<CameraView stream={this.state.myWeb}/>*/}
+                                    <p>{userObj.userId}</p>
                                     <p>{userObj.nickname}</p>
                                 </div>
                             )
                         })}
                     </div> : <p>No mate in room yet</p>}
+                    {store.roomId ?
+                        <div id={"chatInput"}>
+                            <Input
+                                control={"input"}
+                                type={"text"}
+                                label={"Chat"}
+                                onChange={(e) => this.handleMsg(e)}
+                                value={this.state.msg}
+                            />
+                            <Button onClick={this.submitToSockets}>Send</Button>
+                        </div> : null}
                 </div>
                 <div id={store.roomId ? "leaveButton" : "noLeaveButton"}>
                     <div onClick={() => this.leaveRoom()} id={"actualLeaveButton"} className={"bebas"}>Leave room</div>
